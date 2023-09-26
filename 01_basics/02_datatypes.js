dc52d46e5298d48dff3cb90bccdb00e5cf90b518
sachinsakanwar <33538846+sachinsakanwar@users.noreply.github.com>
2023-09-26 18:02:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all js code as newer version
+
+// alert (3 + 3) we are using nodejs, not browser
+
+let name = "sachin"
+let age = 24
+let isLoggedIn = true
+let state;
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
