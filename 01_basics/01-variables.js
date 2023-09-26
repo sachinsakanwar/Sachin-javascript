e9be7e9e83cad21242a59bf2fe8a122bc81264f8
sachinsakanwar <33538846+sachinsakanwar@users.noreply.github.com>
2023-09-26 16:42:35 +0000

    variables file

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let emailId = "sachin@yahoo.com";
+var accountPass = 123567;
+accountCity = "pune";
+let accountState ;
+
+// accountId = 2 // not allowed
+
+accountPass = '2497957';
+accountState = "Mumbai";
+
+console.log(accountState);
+console.table([accountId,accountPass,accountState,accountCity]);
