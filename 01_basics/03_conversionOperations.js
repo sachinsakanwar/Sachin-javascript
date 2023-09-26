84da38f5ca7780b1e6994e6e6163e48ce8cd6ee7
sachinsakanwar <33538846+sachinsakanwar@users.noreply.github.com>
2023-09-26 18:23:17 +0000

    datatype conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,11 @@
+let score = "Sachin"
+let scoreInNumber = Number(score)
+console.log(scoreInNumber); //NaN
+
+let isLoggedIn = "Sac"
+let inBoolean = Boolean(isLoggedIn);
+console.log(inBoolean); //true
+
+// 1 => true; 0 => false
+// "" => false
+// "sachin" => true
